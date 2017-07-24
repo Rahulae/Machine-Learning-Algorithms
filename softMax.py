ea8b421b0241b27048ddec60ef9338d068e6a6a0
Quan Dang <20153005@student.hust.edu.vn>
2017-07-24 20:35:12 +0700

    Add files via upload

---
+++ softMax.py
@@ -0,0 +1,18 @@
+#SOFTMAX FUNCTION
+
+scores = [3.0,1.0,0.2]
+import numpy as np
+import matplotlib.pyplot as plt
+
+def softmax(x):
+  return np.exp(x)/ np.sum(np.exp(x),axis=0)
+
+print(softmax(scores))
+
+#plot softmax curves
+x = np.arange(-2.0,6.0,0.1)
+scores = np.vstack([x, np.ones_like(x),0.2*np.ones_like(x)])
+
+plt.plot(x,softmax(scores).T,linewidth=2)
+plt.show()
+plt.close()
